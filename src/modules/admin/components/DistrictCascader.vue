aaf64de5f1ac47d63afbf73167cbc8b60adac8b9
Huijie Wei <huijiewei@outlook.com>
2020-10-31 19:28:30 +0800

    修复错误

--- src/modules/admin/components/DistrictCascader.vue
+++ src/modules/admin/components/DistrictCascader.vue
@@ -2,7 +2,6 @@
   <el-cascader
     style="width: 100%"
     :placeholder="placeholder"
-    :options="rootDistricts"
     :props="
       Object.assign(
         {
@@ -49,55 +48,32 @@ export default {
     prop: 'value',
     event: 'change',
   },
-  data() {
-    return {
-      rootDistricts: [],
-    }
-  },
-  mounted() {
-    this.getRootDistricts()
-  },
   methods: {
     handleChange(value) {
       this.$emit('change', value)
     },
-    async getDistricts(parentId) {
-      return await MiscService.districts(parentId)
-    },
-    async getRootDistricts() {
-      this.loading = true
-      const { data } = await this.getDistricts(0)
-
-      this.rootDistricts = [
-        ...[
-          {
-            id: 0,
-            name: '全国',
-            code: '0',
-            leaf: true,
-          },
-        ],
-        ...data,
-      ]
-
-      this.loading = false
-    },
     async loadDistricts(node, resolve) {
-      if (node.data && node.data.id) {
-        this.loading = true
+      if (node.data && node.data.leaf) {
+        resolve([])
+        return
+      }
 
-        const { data } = await this.getDistricts(node.data.id)
+      const parentId = node.data && node.data.id ? node.data.id : 0
 
-        if (data) {
-          data.forEach((item) => {
-            item.leaf = item.code.length === this.leafLength
-            item.disabled = this.disabledCodes.includes(item.code)
-          })
+      const { data } = await MiscService.districts(parentId)
 
-          resolve(data)
-        }
+      if (data) {
+        data.forEach((item) => {
+          item.leaf = item.code.length === this.leafLength
+          item.disabled = this.disabledCodes.includes(item.code)
+        })
 
-        this.loading = false
+        const districts =
+          parentId === 0
+            ? [...[{ id: 0, name: '全国', code: '0', leaf: true }], ...data]
+            : data
+
+        resolve(districts)
       }
     },
   },
